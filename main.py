cc125db81c52b283a57006bd28459ce491e1bbc6
GodOfArms <mykhailo.ruzmetov@student.tuke.sk>
2022-11-05 18:32:12 +0100

    main.py is totally usable!

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time, random
-
+import time
 
 start_time = time.time()
 
@@ -9,19 +8,20 @@ start_time = time.time()
 # not numeric sum is always "-"
 # carrying from every not numeric sum is 0
 def find_possible_sum(n1, n2):
-    result = list()
+    sum_result = list()
     carry = 0
     
     for d1, d2 in zip(reversed(n1), reversed(n2)):
         if not (d1.isnumeric() and d2.isnumeric()):
-            result.append('-')
+            sum_result.append('-')
             carry = 0
             continue
         
-        result.append(str(int(d1)+int(d2)+carry)[-1])
+        sum_result.append(str(int(d1)+int(d2)+carry)[-1])
         carry = 0 if int(d1)+int(d2) < 10 else 1
     
-    return ''.join([str(carry)] + list(reversed(result)))
+    first_element = [str(carry)] if sum_result[-1] != '-' else ['-']
+    return ''.join(first_element + list(reversed(sum_result)))
 
 
 # replaces letters in the word with existing numbers in the numbered_letters
@@ -35,7 +35,7 @@ def not_used_numbers(nl):
 
 
 # nl is numbered_letters
-def check_result(word1, word2, result, nl):
+def check_result(nl):
     word1_replaced = substitute_letters(word1, nl)
     word2_replaced = substitute_letters(word2, nl)
     result_replaced = substitute_letters(result, nl)
@@ -43,65 +43,59 @@ def check_result(word1, word2, result, nl):
     words_sum = find_possible_sum(word1_replaced, word2_replaced)
 
     for c1, c2 in zip(result_replaced, words_sum):
-        if c1.isnumeric() and c2.isnumeric() and c1 != c2:
+        if c1.isnumeric() and c2.isnumeric() and c1 != c2 and int(c1) != int(c2)+1:
             return False
     return True
 
 
 # returns all possible values for a specific character c (which are satisfying following condition: word1+word2=result)
-def possible_numbers(letter, word1, word2, result, nl):
+def possible_numbers(letter, nl):
     numbers = list()
     tmp_nl = nl.copy()
 
     for e in not_used_numbers(nl):
         tmp_nl[letter] = e
-        if check_result(word1, word2, result, tmp_nl):
+        if check_result(tmp_nl):
             numbers.append(e)
     
     return numbers
 
 
 # returns the best letter to be replaced with a number (has the least possible values)
-def best_letter(letters, word1, word2, result, nl):
+def best_letter(nl):
     possibilities = dict()
 
     for letter in letters:
         if letter not in nl:
-            possibilities[letter] = possible_numbers(letter, word1, word2, result, nl)
-    best_key = list(reversed(sorted(possibilities, key=lambda x: len(possibilities[x]))))[0]
-
-    print(possibilities)
+            possibilities[letter] = possible_numbers(letter, nl)
+    best_key = sorted(possibilities, key=lambda x: len(possibilities[x]))[0]
     
     return best_key, possibilities[best_key]
 
 
-
-
-
-def smm_solver(letters, word1, word2, result, nl):
-    if not check_result(word1, word2, result, nl):
-        return False
+def smm_solve(nl):
     if len(letters) == len(nl):
-        return True
-    # print(nl)
+        return nl
+    if not check_result(nl):
+        return False
 
     tmp_nl = nl.copy()
-    letter, values = best_letter(letters, word1, word2, result, nl)
+    letter, values = best_letter(nl)
 
     for e in values:
         tmp_nl[letter] = e
-        smm_solver(letters, word1, word2, result, tmp_nl)
+        solved_nl = smm_solve(tmp_nl)
+        if solved_nl:
+            nl = solved_nl
+            return solved_nl
 
 
 if __name__ == '__main__':
     word1 = 'SEND'
     word2 = 'MORE'
     result = 'MONEY'
+    letters = list(set(word1 + word2 + result))
 
-    letters = list(set(result + word1 + word2))
-    numbered_letters = {}
-
-    smm_solver(letters, word1, word2, result, numbered_letters)
-
+    print(smm_solve(dict()))
 
 print(f'Executing time: {round(time.time()-start_time, 5)} seconds')
